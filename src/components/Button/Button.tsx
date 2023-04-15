3b6966af41b3270194f5283cf9560109872ffad7
tskorohod <tskorohod@lmteam.by>
2023-04-15 17:11:08 +0300

    Feat: added choose option using button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,23 @@
-const Button = ({
+export interface ButtonProps {
+	title: string,
+	type: "button" | "submit" | "reset" | undefined,
+	className: string,
+	handleClick?:  React.MouseEventHandler<HTMLButtonElement> | undefined,
+}
+
+export const Button = ({
 	title,
 	type,
-	className
-}) => {
+	className,
+	handleClick,
+}: ButtonProps) => {
+	// const handleClick = (props: Function) => {
+	// 	showList();
+	// };
+
 	return (
 		<>
-			<button className={className} type={ type }>{ title }</button>
+			<button onClick={ handleClick } className={className} type={ type }>{ title }</button>
 		</>
 	)
 }
-
-export default Button;
--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -4,18 +4,29 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { getDataChart } from '../utils/utils';
 import './Chart.css';
-import Button from '../Button/Button';
+import {Button} from '../Button/Button';
 
 export const Chart = (props: any) => {
 	const [minRadius, setMinRadius] = useState<string[]>([]);
 	const [namePlanet, setNamePlanet] = useState<string[]>([]);
+	const [orbitPlanet, setOrbit] = useState<string[]>([]);
+
+	const [titleButton, setTitleButton] = useState("Choose a comparison option");
+
+	const[dataChart, setDataChart] = useState<string[]>([]);
+
+	const [show, setShow] = useState <Boolean>(false);
+
 
 	useEffect(() => {
 		getDataChart().then((data) => {
 			let sizePlane = data.map((planet => { return planet.meanRadius }));
 			let namePlane = data.map((planet => { return planet.englishName }));
+			let sideralOrbitPlane = data.map((planet => { return planet.sideralOrbit }));
+			console.log(data);
 			setMinRadius(sizePlane)
 			setNamePlanet(namePlane);
+			setOrbit(sideralOrbitPlane);
 		})
 	}, []);
 
@@ -35,9 +46,45 @@ export const Chart = (props: any) => {
 		series: [
 			{
 				name: namePlanet,
-				data: minRadius
+				data: dataChart
 			}
 		]
+	};
+
+	const chooseOption = () => {
+		setShow(true);
+	};
+
+	const chooseRadius = () => {
+		setTitleButton("Radius");
+		setShow(false);
+		
+		console.log("data1");
+	};
+
+	const chooseOrbita = () => {
+		setTitleButton("Orbita");
+		setShow(false);
+		
+		console.log("data2");
+	};
+
+	// const optionsSelect = {
+	// 	"Radius": {
+	// 		select: minRadius,
+			
+	// 	},
+	// 	"Option": {
+	// 		select: orbitPlanet,
+	// 	}
+	// }
+
+	const compare = () => {
+		// setDataChart(optionsSelect.titleButton.select);
+		//console.log(titleButton)
+		
+		titleButton === "Radius" ? setDataChart(minRadius) : setDataChart(orbitPlanet);
+		
 	};
 
 	return (
@@ -45,8 +92,17 @@ export const Chart = (props: any) => {
 
 			<div>
 				<div className="blockButton">
-					<Button className="choose" type="button" title="Choose a comparison option"></Button>
-					<Button className="compare" type="button" title="Compare"></Button>
+					<div className="blockList">
+						<Button handleClick={ chooseOption } className={"button choose"} type="button" title={titleButton}></Button>
+						{
+							show &&
+							<ul className="list">
+								<li className="itemList" onClick={ chooseRadius }>Radius</li>
+								<li className="itemList" onClick={ chooseOrbita }>Orbita</li>
+							</ul>
+						}
+					</div>
+					<Button handleClick={ () => compare() } className="button" type="button" title="Compare"></Button>
 				</div>
 				{options ? <div className='chart-box'><HighchartsReact className='chart-box' highcharts={Highcharts} options={options} /> </div> : <div>Loader...</div>}
 			</div>
